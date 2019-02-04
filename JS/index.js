7c25404d569b14c3888cb1dc01549fdb73b3f0a4
Nicole Gunther <nicolemgunther@gmail.com>
2019-02-04 14:31:05 -0800

    added footer layout and JS load events to header/main

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,36 @@
 let btn = document.querySelector('#btn');
+let hello = document.querySelector('.hello');
+let welcome = document.querySelector('.welcome');
+let introText = document.querySelector('.intro-text');
 let mainText = document.querySelectorAll('.main-text');
+
+window.addEventListener('load', () => {
+    setTimeout( () => {
+        hello.classList.remove('hidden');
+        hello.classList.add('fade-in-fwd');
+    }, 500);
+});
+
+window.addEventListener('load', () => {
+    setTimeout( () => {
+        welcome.style.display = "flex";
+        welcome.classList.add('fade-in-fwd');
+    }, 1500);
+});
+
+window.addEventListener('load', () => {
+    setTimeout( () => {
+        introText.classList.remove('hidden');
+        introText.classList.add('fade-in-fwd');
+    }, 3000);
+});
+
+window.addEventListener('load', () => {
+    setTimeout( () => {
+        btn.classList.remove('hidden');
+        btn.classList.add('fade-in-fwd');
+    }, 5000);
+});
 
 btn.addEventListener('click', () => {
     mainText.forEach(element => {
