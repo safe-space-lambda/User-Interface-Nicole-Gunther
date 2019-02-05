543d7e49f0fd4318a82138de450f4a3cd387b646
Nicole Gunther <nicolemgunther@gmail.com>
2019-02-05 10:26:57 -0800

    changed mobile styling, added desktop features
    Aligned header text more evenly, added photo for header when
    width is above 700px. Added tablet and desktop media queries
    and styled header for both media queries

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,7 @@
 let btn = document.querySelector('.intro a');
 let hello = document.querySelector('.hello');
 let welcome = document.querySelector('.welcome');
+let headerImg = document.querySelector('#header-img');
 let introText = document.querySelector('.intro-text');
 let mainText = document.querySelectorAll('.main-text');
 let signUp = document.querySelector('#signup-btn');
@@ -49,3 +50,31 @@ signUp.addEventListener('mousedown', event => {
 signUp.addEventListener('mouseup', event => {
     event.target.style.fontSize = "2.4rem";
 });
+
+var desktopWidth = window.matchMedia("(min-width: 700px)");
+
+if (desktopWidth.matches) {
+    window.addEventListener('load', () => {
+        setTimeout( () => {
+            headerImg.style.display = "flex";
+            headerImg.classList.add('fade-in-fwd');
+            welcome.style.display = "flex";
+            welcome.classList.add('fade-in-fwd');
+        }, 500);
+    });
+
+    window.addEventListener('load', () => {
+        setTimeout( () => {
+            introText.classList.remove('hidden');
+            introText.classList.add('fade-in-fwd');
+        }, 2000);
+    });
+    
+    window.addEventListener('load', () => {
+        setTimeout( () => {
+            btn.classList.remove('hidden');
+            btn.classList.add('fade-in-fwd');
+            btn.classList.add('btn');
+        }, 3500);
+    });
+}
